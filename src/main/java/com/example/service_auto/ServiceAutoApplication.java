c640360502093c2ef21fae0595f1df2a97ba061a
Sorin Gore <soringoredev@gmail.com>
2025-03-04 20:50:31 +0200

    Initial commit.

---
+++ src/main/java/com/example/service_auto/ServiceAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/service_auto/ServiceAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
